5d3faa9e6179e16f37223de51f61d464fa6e3587
Seb <sebbird95@gmail.com>
2021-03-28 18:48:18 +0100

    Introduction to React
    Co-authored-by: Nathan Atherton <Nathanavie@users.noreply.github.com>

---
+++ src/components/Joke.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+const Joke = () => {
+  const [jokeType, setJokeType] = useState();
+  const [setup, setSetup] = useState();
+  const [delivery, setDelivery] = useState();
+  const [singleJoke, setSingleJoke] = useState();
+
+  const fetchJoke = () => {
+    fetch(
+      'https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,religious,nsfw'
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const { type } = res;
+        if (type === 'single') {
+          setJokeType('single');
+          setSingleJoke(res.joke);
+        }
+        if (type === 'twopart') {
+          setJokeType('twopart');
+          setSetup(res.setup);
+          setDelivery(res.delivery);
+        }
+      });
+  };
+
+  if (jokeType === 'single') {
+    return (
+      <>
+        <p>{singleJoke}</p>
+        <Button func={fetchJoke} wording="Get Another Joke" />
+      </>
+    );
+  }
+  if (jokeType === 'twopart') {
+    return (
+      <div className="twopart" id="twopart">
+        <p>{setup}</p>
+        <p>{delivery}</p>
+        <Button func={fetchJoke} wording="Get Another Joke" />
+      </div>
+    );
+  }
+  return <Button func={fetchJoke} wording="Get a Joke" />;
+};
+
+export default Joke;
---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Joke from '../components/Joke';
+
+const MainPage = () => (
+  <>
+    <h1>Joke Generator</h1>
+    <Joke />
+  </>
+);
+
+export default MainPage;
